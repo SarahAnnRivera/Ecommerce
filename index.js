308d8636eb6f927237120fd5f2fd94a8d0f46f64
Sarah Rivera <sarahannrivera25@gmail.com>
2026-06-13 16:19:12 -0400

    Commit Check.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function openMenu() {
-    document.body.classList += " menu--open"
+    document.body.classList += (" menu--open")
 
 }
 
-function closedMenu() {
+function closeMenu() {
     document.body.classList.remove('menu--open')
 }
